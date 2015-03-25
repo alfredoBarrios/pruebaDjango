827c5cc25ee39324827598b90af87d6d8fdcd869
alfredoBarrios <alfbarrios2010@gmail.com>
2015-03-25 11:28:02 -0400

    login

--- pruebaDjango/pruebaDjango/settings.py
+++ pruebaDjango/pruebaDjango/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'UTC'
 
@@ -85,3 +85,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL='/templates/'
